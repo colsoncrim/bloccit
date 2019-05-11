ea0db674cf7cc6fda03fe1bfb75168bea68ff9c6
colsoncrim <colsonscrim@gmail.com>
2019-05-10 19:00:24 -0700

    Post resource 1 checkpoint complete

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,8 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {});
   Topic.associate = function(models) {
     // associations can be defined here
@@ -10,6 +16,14 @@ module.exports = (sequelize, DataTypes) => {
     Topic.hasMany(models.Banner, {
       foreignKey: "topicId",
       as: "banners",
+    });
+    Topic.hasMany(models.Rule, {
+      foreignKey: "topicId",
+      as: "rules",
+    });
+    Topic.hasMany(models.Post, {
+      foreignKey: "topicId",
+      as: "posts"
     });
   };
   return Topic;
