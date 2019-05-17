e78547608cf45a600be519514c4b42474cef6f19
colsoncrim <colsonscrim@gmail.com>
2019-05-17 16:49:59 -0700

    Comments checkpoint complete

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,4 +1,3 @@
-
 module.exports = {
     validatePosts(req, res, next) {
       if(req.method === "POST") {
@@ -17,7 +16,7 @@ module.exports = {
     },
     validateUsers(req, res, next) {
         if(req.method === "POST") {
-   
+
           req.checkBody("email", "must be valid").isEmail();
           req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
           req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
@@ -30,6 +29,20 @@ module.exports = {
           return res.redirect(req.headers.referer);
         } else {
           return next();
+        }
+      },
+      validateComments(req, res, next) {
+        if(req.method === "POST") {
+          req.checkBody("body", "must not be empty"). notEmpty();
+        }
+
+        const errors = req.validationErrors();
+
+        if (errors) {
+          req.flash("error", errors);
+          return res.redirect(req.headers.referer);
+        } else {
+          return next()
         }
       }
   }
